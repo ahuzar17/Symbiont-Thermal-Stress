064edd66f8181c69bd66a609b81929540263c3db
Erica Sun <ericasun@scc-wi3.scc.bu.edu>
2021-04-22 09:22:41 -0400

    add brown GO.R

--- B_psy_brown_GO.R
+++ B_psy_brown_GO.R
@@ -5,7 +5,7 @@ setwd("C:/Users/corey/Downloads/Rscriptanddata/New_Oc9/Gene_Expression/Symbiont-
 
 ###############################################################################
 #WGCNA module turquoise GO division BP
-input="greenyellow_fisher.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
+input="brownbrown_fisher.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
 goAnnotations="B_psygmophilum_isogroup_to_GOterm.tab" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
 goDatabase="go.obo" # download from http://www.geneontology.org/GO.downloads.ontology.shtml
 goDivision="BP" # either MF, or BP, or CC, we ran all 3 individually
@@ -21,7 +21,8 @@ gomwuStats(input, goDatabase, goAnnotations, goDivision,
            #	Alternative="g" # by default the MWU test is two-tailed; specify "g" or "l" of you want to test for "greater" or "less" instead. 
            Module=TRUE,Alternative="g" # un-remark this if you are analyzing a SIGNED WGCNA module (values: 0 for not in module genes, kME for in-module genes). In the call to gomwuPlot below, specify absValue=0.001 (count number of "good genes" that fall into the module)
            #	Module=TRUE # un-remark this if you are analyzing an UNSIGNED WGCNA module 
-) #10 GO terms at 10% FDR
+) 
+#0 GO terms at 10% FDR
 # do not continue if the printout shows that no GO terms pass 10% FDR.
 
 
@@ -40,7 +41,7 @@ results=gomwuPlot(input,goAnnotations,goDivision,
 
 ###############################################################################
 #WGCNA module turquoise GO division MF
-input="greenyellow_fisher.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
+input="brownbrown_fisher.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
 goAnnotations="B_psygmophilum_isogroup_to_GOterm.tab" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
 goDatabase="go.obo" # download from http://www.geneontology.org/GO.downloads.ontology.shtml
 goDivision="MF" # either MF, or BP, or CC, we ran all 3 individually
@@ -56,26 +57,14 @@ gomwuStats(input, goDatabase, goAnnotations, goDivision,
            #	Alternative="g" # by default the MWU test is two-tailed; specify "g" or "l" of you want to test for "greater" or "less" instead. 
            Module=TRUE,Alternative="g" # un-remark this if you are analyzing a SIGNED WGCNA module (values: 0 for not in module genes, kME for in-module genes). In the call to gomwuPlot below, specify absValue=0.001 (count number of "good genes" that fall into the module)
            #	Module=TRUE # un-remark this if you are analyzing an UNSIGNED WGCNA module 
-) #5 GO terms at 10% FDR
+) #0 GO terms at 10% FDR
 # do not continue if the printout shows that no GO terms pass 10% FDR.
 
 
-# Plotting results
-
-results=gomwuPlot(input,goAnnotations,goDivision,
-                  #	absValue=-log(0.05,10),  # genes with the measure value exceeding this will be counted as "good genes". Specify absValue=0.001 if you are doing Fisher's exact test for standard GO enrichment or analyzing a WGCNA module (all non-zero genes = "good genes").
-                  absValue=0.001,
-                  level1=0.1, # FDR threshold for plotting. Specify level1=1 to plot all GO categories containing genes exceeding the absValue.
-                  level2=0.05, # FDR cutoff to print in regular (not italic) font.
-                  level3=0.01, # FDR cutoff to print in large bold font.
-                  txtsize=1.2,    # decrease to fit more on one page, or increase (after rescaling the plot so the tree fits the text) for better "word cloud" effect
-                  treeHeight=0.5, # height of the hierarchical clustering tree
-                  #	colors=c("dodgerblue2","firebrick1","skyblue2","lightcoral") # these are default colors, un-remar and change if needed
-)
 
 ###############################################################################
 #WGCNA module turquoise GO division CC
-input="greenyellow_fisher.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
+input="brownbrown_fisher.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
 goAnnotations="B_psygmophilum_isogroup_to_GOterm.tab" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
 goDatabase="go.obo" # download from http://www.geneontology.org/GO.downloads.ontology.shtml
 goDivision="CC" # either MF, or BP, or CC, we ran all 3 individually
@@ -91,7 +80,7 @@ gomwuStats(input, goDatabase, goAnnotations, goDivision,
            #	Alternative="g" # by default the MWU test is two-tailed; specify "g" or "l" of you want to test for "greater" or "less" instead. 
            Module=TRUE,Alternative="g" # un-remark this if you are analyzing a SIGNED WGCNA module (values: 0 for not in module genes, kME for in-module genes). In the call to gomwuPlot below, specify absValue=0.001 (count number of "good genes" that fall into the module)
            #	Module=TRUE # un-remark this if you are analyzing an UNSIGNED WGCNA module 
-) #10 GO terms at 10% FDR
+) #5 GO terms at 10% FDR
 # do not continue if the printout shows that no GO terms pass 10% FDR.
 
 
